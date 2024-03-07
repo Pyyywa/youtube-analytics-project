937af1e6166a1c889480cae1934266c18aec09b7
Rusha <pyyywa@gmail.com>
2024-03-07 20:52:13 +0300

    hw3 done

--- src/channel.py
+++ src/channel.py
@@ -21,12 +21,44 @@ class Channel:
         self.title = self.youtube['items'][0]['snippet']['title']
         self.description = self.youtube["items"][0]["snippet"]["description"]
         self.url = self.youtube["items"][0]["snippet"]["thumbnails"]["high"]["url"]
-        self.subscriber_count = self.youtube["items"][0]["statistics"]["subscriberCount"]
+        self.subscriber_count = int(self.youtube["items"][0]["statistics"]["subscriberCount"])
         self.video_count = self.youtube["items"][0]["statistics"]["videoCount"]
         self.view_count = self.youtube["items"][0]["statistics"]["viewCount"]
         self.data = {"title": self.title, "description": self.description,
 "url": self.url,"subscriber_count": self.subscriber_count,
 "video_count": self.video_count,"view_count": self.view_count}
+
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+
+    def __add__(self, other):
+        return (self.subscriber_count + other.subscriber_count)
+
+
+    def __sub__(self, other):
+        return (self.subscriber_count - other.subscriber_count)
+
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
+
+
+    def __eq__(self, other):
+        return self.subscriber_count == other.subscriber_count
 
 
     def print_info(self) -> None:
@@ -49,5 +81,5 @@ part = 'snippet,statistics').execute()
         метод, сохраняющий в файл значения атрибутов экземпляра `Channel`
         """
         with open(filename, "w") as file:
-            json.dump(self.youtube, file, indent = 2, ensure_ascii =False)
+            json.dump(self.youtube, file, indent = 2, ensure_ascii = False)
 
