24ae89ab11a4b7796f22a4d7d10d15774a7f3ece
Rusha <pyyywa@gmail.com>
2024-03-14 18:31:38 +0300

    hw6

--- src/video.py
+++ src/video.py
@@ -9,20 +9,27 @@ youtube = build('youtube', 'v3', developerKey = api_key)
 class Video():
 
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=video_id
-                                                        ).execute()
+        try:
+            self.video_id = video_id
+            self.video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=video_id
+                                                            ).execute()
 
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url = self.video_response["items"][0]["snippet"]["thumbnails"]["high"]["url"]
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.url = self.video_response["items"][0]["snippet"]["thumbnails"]["high"]["url"]
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
 
     def __str__(self):
 
-        return f"{self.video_title}"
+        return f"{self.title}"
 
 
 class PLVideo(Video):
